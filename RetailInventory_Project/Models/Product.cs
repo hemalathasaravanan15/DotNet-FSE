d2fa9020432eb999a208fd6c6a65431ee78f32c2
Hemalatha saravanan <hemzsar@gmail.com>
2025-07-05 19:49:53 +0530

    Add files via upload

---
+++ RetailInventory_Project/Models/Product.cs
@@ -0,0 +1,10 @@
+namespace RetailInventory.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Stock { get; set; }
+        public Category Category { get; set; } = null!;
+    }
+}
